ebdee219345d3feb4d1a7cb4b4d0bf573f9ea90a
matt <mdeloux@gmail.com>
2022-01-11 12:57:21 -0600

    commit tree 13941ab49d3860f382ee6cf978216ddfa0bc3977

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(sport = 'roller-skate') {
+    return (`This Saturday, I want to ${sport}!`);
+}
+
+
+const mondayWork = function (work = 'go to the office') {
+    return (`This Monday, I will ${work}.`)
+}
+
+mondayWork();
+
+
+const wrapAdjective = function (style) {
+    return function (descriptor) {
+        return (`You are ${style}${descriptor}${style}!`);
+
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
